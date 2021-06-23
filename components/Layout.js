46bb83a16724c5bf620df7c53a19fcddaaecf156
ryanaccelrobotics <rrivera@accelrobotics.com>
2021-06-23 07:15:03 -0700

    Add google analytics

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,16 @@ const Layout = ({ children, menu }) => {
     <div>
       <Head>
         <title>Accel Robotics</title>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-103888794-1', 'auto');
+            ga('send', 'pageview');
+        ` }} />
       </Head>
       <Header menu={menu} />
       <main className="main">{children}</main>
